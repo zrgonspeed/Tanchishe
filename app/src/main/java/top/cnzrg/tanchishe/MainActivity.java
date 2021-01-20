b8eee42b091ac0a5133e5a0fa884585d6b89de52
zhangriguang <zhangriguang@chuhui.com>
2021-01-20 18:31:50 +0800

    v1.4.10 横屏。

--- app/src/main/java/top/cnzrg/tanchishe/MainActivity.java
+++ app/src/main/java/top/cnzrg/tanchishe/MainActivity.java
@@ -1,18 +1,39 @@
 package top.cnzrg.tanchishe;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import top.cnzrg.tanchishe.param.GameData;
+import top.cnzrg.tanchishe.util.DebugUtils;
+import top.cnzrg.tanchishe.util.Logger;
+import top.cnzrg.tanchishe.util.ToastUtils;
+import top.cnzrg.tanchishe.util.WindowUtils;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //隐藏虚拟按键，并且全屏
+        WindowUtils.hideBottomUIMenu(getWindow());
+
+        /**
+         * 设置为横屏
+         */
+        if (getRequestedOrientation() != ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
 
         Button bt_start = findViewById(R.id.bt_start);
         bt_start.setOnClickListener(new View.OnClickListener() {
